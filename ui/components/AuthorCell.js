d5ef27adbfbf738ef4f4ab1300e591e64915245f
Dave Weaver <dave@markkup.com>
2018-02-23 15:06:40 -0500

    added author cell

---
+++ ui/components/AuthorCell.js
@@ -0,0 +1,37 @@
+export default class AuthorCell extends Component {
+
+	render() {
+        const { item } = this.props;
+
+        // shorten bio
+        let bio = item.bio
+        if (bio.length > 150) {
+            bio = bio.substring(0, 150) + '...'
+        }
+
+        return (
+            <TouchableOpacity onPress={this.props.onPress} activeOpacity={0.9}>
+                <Row spacing={10} style={{backgroundColor:'#fff'}}>
+                    <Cell shrink>
+                        <Avatar medium rounded source={{ uri: item.avatar }} />
+                    </Cell>
+                    <Cell>
+                        <Title left bolder upper>{item.name}</Title>
+                        <Text>{bio}</Text>
+                    </Cell>
+                </Row>
+                <Divider full height={1} />
+            </TouchableOpacity>
+        )
+    }
+
+}
+
+let styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	text: {
+		fontSize: 20
+	}
+})
